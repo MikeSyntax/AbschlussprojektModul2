14a7ea16063afd7dd613fa7da7380e79d085083e
MikeSyntax <autoglasgold@gmail.com>
2023-08-14 15:05:53 +0200

    Abschlussprojekt 14.08 gegen 15.05Uhr

--- src/main/kotlin/CasinoPlayer.kt
+++ src/main/kotlin/CasinoPlayer.kt
@@ -1,148 +1,194 @@
+// Importieren der benötigten Bibliotheken
 import kotlin.math.round
-
+// Definition der Klasse CasinoPlayer, die von der Klasse Casino erbt
 class CasinoPlayer(tableNumber: Int, val name: String, var cash: Double, var drink: Boolean, var tip: Double, var age: Int): Casino(tableNumber){
 
-    var yourNumber: Int = -1                                                                 //Hier werden die gewählten Zahlen der Spieler abgespeichert um später die Gewinne zu berechnen
-    var yourAmount: Int = -1
-    var redBlack: Int = -1
-    var evenOrNot: Int = -1
-    var thirdThird: Int = -1
-    var half: Int = -1
-    var skipRound = false
 
+    // Deklaration von Eigenschaften, um Spielerinformationen zu speichern
+    var yourNumber: Int = -1    // Gewählte Zahl des Spielers
+    var yourAmount: Int = -1    // Gewählter Betrag des Spielers
+    var redBlack: Int = -1      // Wahl von Rot (0) oder Schwarz (1)
+    var evenOrNot: Int = -1     // Wahl von Gerade (0) oder Ungerade (1)
+    var thirdThird: Int = -1    // Wahl von Erstes Drittel (0), Zweites Drittel (1) oder Drittes Drittel (2)
+    var half: Int = -1          // Wahl von Erste Hälfte (0) oder Zweite Hälfte (1)
+    var skipRound = false       // Flag, um eine Runde zu überspringen
     /*init {
         println("Der Spieler $name wurde initialisiert. $name hat $cash € und er hat ein Getränk $drink")
     }*/
 
+    // Sekundärer Konstruktor, der den primären Konstruktor aufruft und einen zusätzlichen Parameter hat
     constructor(tableNumber: Int, name: String, cash: Double, drink: Boolean, tip: Double, age: Int, highRoller: Boolean) : this(tableNumber, name, cash, drink, tip, age) {
         //println("Spieler $name wurde mit dem sekundären Konstruktor initialisiert und ist ein HighRoller $highRoller")
     }
 
+    // Funktion zur Ermittlung des Gewinns basierend auf der gewählten Zahl und der Roulette-Tabelle
     fun getWin(number: Int, rouletteTable: RouletteTable): String {
         when {
+            // Wenn die gewählte Zahl mit der vom Spieler gewählten Zahl übereinstimmt
             (yourNumber == number) -> {
+                // Erhöhe den Kontostand des Spielers um das Gewinnbetrag * 36
                 cash += (yourAmount * 36)
                 var winAmount = yourAmount * 36
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
             }
+            // Wenn die gewählte Zahl rot ist und der Spieler "Schwarz" gewählt hat
             (redNumbers.contains(number) && redBlack == 1) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl schwarz ist und der Spieler "Schwarz" gewählt hat
             (blackNumbers.contains(number) && redBlack == 2) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl gerade ist und der Spieler "Gerade" gewählt hat
             (numberIsEven.contains(number) && evenOrNot == 1) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl ungerade ist und der Spieler "Ungerade" gewählt hat
             (numberIsNotEven.contains(number) && evenOrNot == 2) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl in der ersten Hälfte liegt und der Spieler "1. Drittel" gewählt hat
             (first.contains(number) && thirdThird == 1) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 3
                 cash += (yourAmount * 3)
                 var winAmount = yourAmount * 3
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl in der zweiten Hälfte liegt und der Spieler "2. Drittel" gewählt hat
             (second.contains(number) && thirdThird == 2) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 3
                 cash += (yourAmount * 3)
                 var winAmount = yourAmount * 3
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl in der dritten Hälfte liegt und der Spieler "3. Drittel" gewählt hat
             (third.contains(number) && thirdThird == 3) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 3
                 cash += (yourAmount * 3)
                 var winAmount = yourAmount * 3
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl in der ersten Hälfte liegt und der Spieler "1. Hälfte" gewählt hat
             (firstHalf.contains(number) && half == 1) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die gewählte Zahl in der zweiten Hälfte liegt und der Spieler "2. Hälfte" gewählt hat
             (secondHalf.contains(number) && half == 2) -> {
+                // Erhöhe den Kontostand des Spielers um den Gewinnbetrag * 2
                 cash += (yourAmount * 2)
                 var winAmount = yourAmount * 2
                 var tipAmount = (winAmount - yourAmount) * tip
                 cash -= tipAmount
+                // Trinkgeld für die Angestellten erhöhen und den Bankbetrag der Tabelle reduzieren
                 rouletteTable.tipForEmployees = rouletteTable.tipForEmployees + tipAmount
                 rouletteTable.bankTable = rouletteTable.bankTable - winAmount
                 Thread.sleep(1000)
+                // Rückgabe einer Nachricht über den Gewinn und den aktualisierten Kontostand
                 return "${FontColors.GREEN.type}${(winAmount - yourAmount)}€${FontColors.COLOREND.type} gewonnen! Neuer Cashbestand abzügl. ${FontColors.GREEN.type}${round(tipAmount*100)/100}€${FontColors.COLOREND.type} Trinkgeld ergibt ${FontColors.GREEN.type}${round(cash*100)/100}€${FontColors.COLOREND.type}"
-
             }
+
+            // Wenn die skipRound true gesetzt ist
             (skipRound) -> {
                 Thread.sleep(1000)
                 return  "für diese Runde ausgesetzt"
             }
 
+            // Andernfalls (wenn keine der vorherigen Bedingungen erfüllt ist)
             else -> {
                 Thread.sleep(1000)
                 return "leider ${FontColors.RED.type}verloren${FontColors.COLOREND.type}"
             }
         }
     }
-
+    // Setzt die Spieler-Variablen auf ihre Standardwerte zurück
     fun reset(){
-        yourNumber = -1                                           //zurücksetzen der Variablen aus der CasinoPlayer Klasse
+        yourNumber = -1
         yourAmount= -1
         redBlack= -1
         evenOrNot = -1
@@ -151,6 +197,7 @@ class CasinoPlayer(tableNumber: Int, val name: String, var cash: Double, var dri
         skipRound = false
     }
 
+    // Gibt eine Einführungsnachricht für den Spieler aus
     fun introduce() {
         println("Hallo, ich bin $name und spiele an Tisch $tableNumber")
         Thread.sleep(1000)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,92 +1,145 @@
 fun main() {
     println("")
-    println("${FontColors.GREEN.type}          d8888b   d8888b  d88888b          d8888b     d8b    d8b   db  db   dD        \n" +
-            "          88   8D    88    88               88   8D  d8   8b  888o  88  88  8P         \n" +
-            "          88   88    88    88888            88oooY   88ooo88  88V8o 88  88 8P          \n" +
-            "          88   88    88    88               88   b   88   88  88 V8o88  88 8b          \n" +
-            "          88   8D    88    88               88   8D  88   88  88  V888  88  88         \n" +
-            "          Y8888Y   Y8888Y  Y88888Y          Y8888P   YP   YP  VP   V8P  YP   YD        \n" +
-            "                                                                                       \n" +
-            "                                                                                       \n" +
-            "           d888b   d88888b  db   d8b   db  d8888b  d8b   db  d8b   db  d888888b        \n" +
-            "          88  88b  88       88   I8I   88    88    888o  88  888o  88     88           \n" +
-            "          88       8888888  88   I8I   88    88    88V8o 88  88V8o 88     88           \n" +
-            "          88  888  88       Y8   I8I   88    88    88 V8o88  88 V8o88     88           \n" +
-            "          88   8   88        8b d8 8b d8     88    88  V888  88  V888     88           \n" +
-            "           Y888P   Y88888P    8b8   8d8    Y8888P  VP   V8P  VP   V8P     YP           \n" +
-            "                                                                                         ${FontColors.COLOREND.type}")
+    println(
+        "${FontColors.GREEN.type}          d8888b   d8888b  d88888b          d8888b     d8b    d8b   db  db   dD        \n" +
+                "          88   8D    88    88               88   8D  d8   8b  888o  88  88  8P         \n" +
+                "          88   88    88    88888            88oooY   88ooo88  88V8o 88  88 8P          \n" +
+                "          88   88    88    88               88   b   88   88  88 V8o88  88 8b          \n" +
+                "          88   8D    88    88               88   8D  88   88  88  V888  88  88         \n" +
+                "          Y8888Y   Y8888Y  Y88888Y          Y8888P   YP   YP  VP   V8P  YP   YD        \n" +
+                "                                                                                       \n" +
+                "                                                                                       \n" +
+                "           d888b   d88888b  db   d8b   db  d8888b  d8b   db  d8b   db  d888888b        \n" +
+                "          88  88b  88       88   I8I   88    88    888o  88  888o  88     88           \n" +
+                "          88       8888888  88   I8I   88    88    88V8o 88  88V8o 88     88           \n" +
+                "          88  888  88       Y8   I8I   88    88    88 V8o88  88 V8o88     88           \n" +
+                "          88   8   88        8b d8 8b d8     88    88  V888  88  V888     88           \n" +
+                "           Y888P   Y88888P    8b8   8d8    Y8888P  VP   V8P  VP   V8P     YP           \n" +
+                "                                                                                         ${FontColors.COLOREND.type}"
+    )
 
-
+    // Wartepause von 1 Sekunde
     Thread.sleep(1000)
+
+    // Erstellung eines RouletteTable-Objekts für Tisch 1 mit Croupier Dieter
     var rouletteAtTable1: RouletteTable = RouletteTable(1, "Dieter", 0.00)
+
+    // Erstellung von CasinoEmployees-Objekten für Croupiers Dieter, Rainer und Ernst
     var dieter: CasinoEmployees = CasinoEmployees(1, "Dieter", "Croupier", 36)
-    var rainer: CasinoEmployees = CasinoEmployees(1,"Rainer", "Croupier", 48)
-    var ernst: CasinoEmployees = CasinoEmployees(1,"Ernst", "Croupier", 31)
+    var rainer: CasinoEmployees = CasinoEmployees(1, "Rainer", "Croupier", 48)
+    var ernst: CasinoEmployees = CasinoEmployees(1, "Ernst", "Croupier", 31)
+
+    // Ausgabe von Informationen über den Roulette-Tisch 1
     rouletteAtTable1.printInfo()
 
+    // Ausgabe einer Leerzeile
     println("")
 
-    var rouletteAtTable2: RouletteTable = RouletteTable(2, "Sascha", 0.00, 500000.00, 1000.00, 10000.00)                       //Vorbereitung für den zweiten Tisch
+    // Erstellung eines RouletteTable-Objekts für Tisch 2 mit Croupier Sascha und vorbereiteten Geld- und Trinkgeldbeträgen
+    var rouletteAtTable2: RouletteTable = RouletteTable(2, "Sascha", 0.00, 500000.00, 1000.00, 10000.00)
+
+    // Erstellung von CasinoHighRollerEmployees-Objekten für Croupiers Sascha und Christian
     var sascha: CasinoHighRollerEmployees = CasinoHighRollerEmployees(2, "Sascha", "Croupier", 45)
-    var christian: CasinoHighRollerEmployees = CasinoHighRollerEmployees(2,"Christian", "Croupier", 57)
+    var christian: CasinoHighRollerEmployees = CasinoHighRollerEmployees(2, "Christian", "Croupier", 57)
+
+    // Ausgabe von Informationen über den Roulette-Tisch 2
     rouletteAtTable2.printInfo()
 
-    var rouletteGames: RouletteGames = RouletteGames(1)                         //Instanziierung der RouletteGames an Tisch 1
-    var rouletteGamesHighRoller: RouletteHighRoller = RouletteHighRoller(2)     //Instanziierung der RouletteGames an Tisch 2
+    // Instanziierung eines RouletteGames-Objekts an Tisch 1
+    var rouletteGames: RouletteGames = RouletteGames(1)
 
-    var klaus: CasinoPlayer = CasinoPlayer(1, "Klaus", 500.00, true, 0.01,23)
-    var tatjana: CasinoPlayer = CasinoPlayer(1, "Tatjana", 500.00, true, 0.01,59)
-    var mike: CasinoPlayer = CasinoPlayer(1, "Mike", 500.00, true, 0.02,45)
-    var salva: CasinoPlayer = CasinoPlayer(1, "Salva", 500.00, false, 0.04,71)
-    var claudia: CasinoPlayer = CasinoPlayer(1, "Claudia", 500.00, false, 0.03,26)
-    var enrico: CasinoPlayer = CasinoPlayer(1,"Enrico", 50.00, false, 0.00, 19)
+    // Instanziierung eines RouletteHighRoller-Objekts an Tisch 2
+    var rouletteGamesHighRoller: RouletteHighRoller = RouletteHighRoller(2)
 
-    var gonzales: CasinoPlayer = CasinoPlayer(2, "Gonzales", 10000.00, true, 0.1, 26,true)
-    var silvio: CasinoPlayer = CasinoPlayer(2, "Silvio", 10000.00, true, 0.08, 65,true)
+    // Erstellung von CasinoPlayer-Objekten für verschiedene Spieler
+    var klaus: CasinoPlayer = CasinoPlayer(1, "Klaus", 500.00, true, 0.01, 23)
+    var tatjana: CasinoPlayer = CasinoPlayer(1, "Tatjana", 500.00, true, 0.01, 59)
+    var mike: CasinoPlayer = CasinoPlayer(1, "Mike", 500.00, true, 0.02, 45)
+    var salva: CasinoPlayer = CasinoPlayer(1, "Salva", 500.00, false, 0.04, 71)
+    var claudia: CasinoPlayer = CasinoPlayer(1, "Claudia", 500.00, false, 0.03, 26)
+    var enrico: CasinoPlayer = CasinoPlayer(1, "Enrico", 50.00, false, 0.00, 19)
 
-    val entrance : CasinoEntranceControl = CasinoEntranceControl()
+    // Erstellung von CasinoPlayer-Objekten für High Roller Spieler
+    var gonzales: CasinoPlayer = CasinoPlayer(2, "Gonzales", 10000.00, true, 0.1, 26, true)
+    var silvio: CasinoPlayer = CasinoPlayer(2, "Silvio", 10000.00, true, 0.08, 65, true)
+
+    // Erstellung eines CasinoEntranceControl-Objekts für die Einlasskontrolle
+    val entrance: CasinoEntranceControl = CasinoEntranceControl()
+
+    // Liste von CasinoPlayer-Objekten, die Einlass ins Casino wollen
     val playersWantEntrance: MutableList<CasinoPlayer> = mutableListOf(klaus, tatjana, enrico, mike, salva, claudia)
-    val players: MutableList<CasinoPlayer> = mutableListOf()
-    val playersHighRoller: MutableList<CasinoPlayer> = mutableListOf(gonzales, silvio)
-    val croupiers: MutableList<CasinoEmployees> = mutableListOf(rainer, ernst, dieter)
-    val highRollerCroupiers : MutableList<CasinoHighRollerEmployees> = mutableListOf(christian, sascha)
 
+    // Leere Liste von CasinoPlayer-Objekten für den tatsächlichen Einlass
+    val players: MutableList<CasinoPlayer> = mutableListOf()
+
+    // Liste von High Roller CasinoPlayer-Objekten für den Einlass
+    val playersHighRoller: MutableList<CasinoPlayer> = mutableListOf(gonzales, silvio)
+
+    // Liste von CasinoEmployees-Objekten, die Croupiers am Tisch
+    val croupiers: MutableList<CasinoEmployees> = mutableListOf(rainer, ernst, dieter)
+
+    // Liste von High Roller CasinoHighRollerEmployees-Objekten für die High Roller Tische
+    val highRollerCroupiers: MutableList<CasinoHighRollerEmployees> = mutableListOf(christian, sascha)
+
+    // Ausgabe einer Leerzeile
     println("")
+
+    // Ausgabe einer Meldung über die Einlasskontrolle und Altersprüfung
     println("Hier wird die ${FontColors.RED.type}Einlasskontrolle${FontColors.COLOREND.type} durchgeführt und das Alter geprüft")
+
+    // Schleife durch die Liste von Spielern, die Einlass wollen, und Aufruf der Einlasskontrolle
     for (player in playersWantEntrance) {
         entrance.entranceControl(player, players)
     }
 
+    // Ausgabe einer Leerzeile
     println("")
+
+    // Schleife durch die Liste von Spielern am Tisch 1 und Aufruf der introduce()-Methode
     println("${FontColors.YELLOW.type}Tisch Nummer ${rouletteAtTable1.tableNumber}${FontColors.COLOREND.type}")
     for (player in players) {
         player.introduce()
     }
 
+    // Ausgabe einer Leerzeile
     println("")
+
+    // Schleife durch die Liste von High Roller Spielern am Tisch 2 und Aufruf der introduce()-Methode
     println("${FontColors.YELLOW.type}Tisch Nummer ${rouletteAtTable2.tableNumber}${FontColors.COLOREND.type}")
     for (player in playersHighRoller) {
         player.introduce()
     }
 
+    // Ausgabe einer Leerzeile
     println("")
 
+    // Schleife, die das Spiel abwickelt, solange goOn1 oder goOn2 wahr ist
     do {
+        // Spielen am Tisch 1
         rouletteAtTable1.goPlay(players, rouletteGames, rouletteAtTable1)
+        // Entfernen von Spielern ohne Geld am Tisch 1
         rouletteAtTable1.removePlayerWithNoMoney(players)
 
+        // Spielen am High Roller Tisch 2
         rouletteAtTable2.goPlayHighRoller(playersHighRoller, rouletteGamesHighRoller, rouletteAtTable2)
+        // Entfernen von High Roller Spielern ohne Geld am Tisch 2
         rouletteAtTable2.removePlayerHighRollerWithNoMoney(playersHighRoller)
 
+        // Erhöhen der Runde am Tisch 1
         round++
+        // Ändern des Croupiers am Tisch 1
         dieter.changeCroupier(croupiers, rouletteAtTable1)
+
+        // Erhöhen der Runde am High Roller Tisch 2
         roundHighRoller++
+        // Ändern des High Roller Croupiers am Tisch 2
         sascha.changeHighRollerCroupier(highRollerCroupiers, rouletteAtTable2)
 
+        // Die Schleife wird ausgeführt, solange goOn1 oder goOn2 wahr ist
     } while (goOn1 || goOn2)
 
-        println("${FontColors.RED.type}´`´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´")
-        println("| Alle Spieler haben ihr Geld verspielt, das Spiel ist vorbei |")
-        println("´`´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´${FontColors.COLOREND.type}")
-
+    // Ausgabe einer gestalteten Meldung, wenn das Spiel vorbei ist
+    println("${FontColors.RED.type}´`´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´")
+    println("| Alle Spieler haben ihr Geld verspielt, das Spiel ist vorbei |")
+    println("´`´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´`´`´`´``´`´`´`´`´`´`´`´${FontColors.COLOREND.type}")
 }
